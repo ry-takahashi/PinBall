26d62e107f033330ae1cac6119696aebbe975fa7
髙橋　綾太 <ry-takahashi@i-enter.co.jp>
2019-04-11 11:29:21 +0900

    PinBallプロジェクト 初めてのコミット
    スマートフォン向けのピンボールゲームを作成致しました。

---
+++ Assets/FripperController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperController : MonoBehaviour {
+
+	// HingeJointコンポーネントを入れる
+	private HingeJoint myHingeJoint;
+
+	// 初期の傾き
+	private float defaultAngle = 20;
+
+	// 弾いた時の傾き
+	private float flickAngle = -20;
+
+	// Use this for initialization
+	void Start () {
+		// HingeJointコンポーネント取得
+		this.myHingeJoint = GetComponent<HingeJoint>();
+
+		// フリッパーの傾きを設定
+		SetAngle( this.defaultAngle );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.LeftArrow) &&
+		   tag == "LeftFripperTag") {
+			SetAngle (this.flickAngle);
+		}
+
+		if (Input.GetKeyDown (KeyCode.RightArrow) &&
+			tag == "RightFripperTag") {
+			SetAngle (this.flickAngle);
+		}
+
+		if (Input.GetKeyUp (KeyCode.LeftArrow) &&
+			tag == "LeftFripperTag") {
+			SetAngle (this.defaultAngle);
+		}
+
+		if (Input.GetKeyUp (KeyCode.RightArrow) &&
+			tag == "RightFripperTag") {
+			SetAngle (this.defaultAngle);
+		}
+
+		if (Input.touchCount > 0) {
+			if (Input.GetTouch (0).phase == TouchPhase.Began) {
+				Debug.Log ("触ってるよ");
+			}
+		}
+
+
+
+
+	}
+
+	// フリッパーの傾きを設定
+	public void SetAngle( float Angle ) {
+		JointSpring jointSpr = this.myHingeJoint.spring;
+		jointSpr.targetPosition = Angle;
+		this.myHingeJoint.spring = jointSpr;
+	}
+
+}
---
+++ Assets/myScore.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myScore : MonoBehaviour {
+
+	[SerializeField]
+	private int score;
+
+	private GameObject scoreText;
+
+	// Use this for initialization
+	void Start () {
+
+		this.scoreText = GameObject.Find("ScoreText");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter (Collision other)
+	{
+		this.scoreText.GetComponent<ScoreController> ().AddScore (score);
+	}
+}
