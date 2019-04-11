39e85def44d5ab3c20555a7fe71e86442ef67ed3
髙橋　綾太 <ry-takahashi@i-enter.co.jp>
2019-04-11 15:30:55 +0900

    PinBallプロジェクト 課題再提出
    タグによってターゲットの種類を判別するようにmyScore.csを修正致しました。

--- Assets/myScore.cs
+++ Assets/myScore.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class myScore : MonoBehaviour {
 
-	[SerializeField]
 	private int score;
 
 	private GameObject scoreText;
 
 	// Use this for initialization
 	void Start () {
-
 		this.scoreText = GameObject.Find("ScoreText");
+
+		// タグによってスコアを設定
+		if (tag == "SmallStarTag") {
+			score = 10;
+
+		} else if (tag == "LargeStarTag") {
+			score = 40;
+
+		} else if (tag == "SmallCloudTag") {
+			score = 20;
+
+		} else if (tag == "LargeCloudTag") {
+			score = 50;
+		}
 	}
 	
 	// Update is called once per frame
